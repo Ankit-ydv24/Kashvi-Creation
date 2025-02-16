a22fb6aeb10bc96cf2c3380ef4c6ad7e885f2d57
Shreyansh-023 <shreyanshmishra313@gmail.com>
2025-02-16 11:15:04 +0530

    Add

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\GWOC\Kashvi-Creation\venv
+set VIRTUAL_ENV=C:\new pc\Gwoc 2\Kashvi-Creation\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
